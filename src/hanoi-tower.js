d5c504ce56cc6d9f882e3d76f1a71113f08302a5
roshik14 <fraust1337@gmail.com>
2021-02-23 21:40:36 +0700

    feat: add solution in hanoi tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/*disksNumber, turnsSpeed*/) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = ((2 ** disksNumber) - 1);
+  const seconds =  Math.floor(turns * 3600 / turnsSpeed);
+  return {
+    turns: turns,
+    seconds: seconds
+  }
 };
