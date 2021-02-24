a6282e53d4a9b2c57a73a362b4ae22f493a9e0f9
roshik14 <fraust1337@gmail.com>
2021-02-25 00:00:20 +0700

    feat: add some solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined)
+      throw new Error("No one of parameters");
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let fullKey = "";
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let cur = 0; key.length < message.length; cur++) {
+      key += key[cur];
+    }
+    for (let i = 0, j = 0; i < message.length; i++, j++) {
+      let c = 0;
+      if (message[i] === " ") {
+        fullKey += " ";
+        j -= 1;
+        continue;
+      }
+      if (!alphabet.includes(message[i])) {
+        fullKey += message[i];
+        continue;
+      }
+      c =
+        (alphabet.indexOf(message[i]) + alphabet.indexOf(key[j])) %
+        alphabet.length;
+      fullKey += alphabet[c];
+    }
+    return fullKey;
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined)
+      throw new Error("No one of parameters");
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let fullKey = "";
+    key = key.toUpperCase();
+    for (let cur = 0; key.length < encryptedMessage.length; cur++) {
+      key += key[cur];
+    }
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++, j++) {
+      let m = 0;
+      if (encryptedMessage[i] === " ") {
+        fullKey += " ";
+        j -= 1;
+        continue;
+      }
+      if (!alphabet.includes(encryptedMessage[i])) {
+        fullKey += encryptedMessage[i];
+        continue;
+      }
+      m =
+        (alphabet.indexOf(encryptedMessage[i]) +
+          alphabet.length -
+          alphabet.indexOf(key[j])) %
+        alphabet.length;
+      fullKey += alphabet[m];
+    }
+    return fullKey;
   }
 }
 
