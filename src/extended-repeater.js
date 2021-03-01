cdb995d2f908e7e74846dcd8ca6005535d30efbc
roshik14 <fraust1337@gmail.com>
2021-03-02 01:19:48 +0700

    fix: change solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,24 +5,36 @@ module.exports = function repeater(str, options) {
     str = String(str);
     options.addition = String(options.addition);
   }
-  function addRep(string) {
+  function addRep(string = '') {
     let arr = [];
+    if (options.additionRepeatTimes === undefined) {
+       arr.push(string);
+    } else {
       for (let i = 0; i < options.additionRepeatTimes; i++) {
         arr.push(string);
       }
+    }
     return arr;
   }
   function sepAdd(string = '|') {
     return addRep(options.addition).join(string);
   }
   function concat(string) {
-    return string + sepAdd(options.additionSeparator);
+    const additionSep = sepAdd(options.additionSeparator);
+    if (additionSep === 'undefined') {
+      return string;
+    }
+    return string + additionSep;
   }
   function lastSep(string = '+') {
     let newArr = [];
+    if (options.repeatTimes === undefined) {
+      newArr.push(concat(str));
+    } else {
       for (let i = 0; i < options.repeatTimes; i++) {
         newArr.push(concat(str));
       }
+    }
     return newArr.join(string);
   }
   return lastSep(options.separator);
