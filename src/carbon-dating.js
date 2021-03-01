09fd72ba40c2c9162bbb486020552e74ae469cdb
roshik14 <fraust1337@gmail.com>
2021-03-02 01:49:58 +0700

    fix: update tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,21 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity !== "string") {
 	return false;
   }
-  let num = parseInt(sampleActivity, 10);
+  let num = parseFloat(sampleActivity, 10);
   if (sampleActivity === undefined || num === "NaN") {
 	  return false;
   }
   if (num > 0 && num < 15) {
   	let act = MODERN_ACTIVITY / num;
-  	let k = 0.693 / HALF_LIFE_PERIOD;
+  	let k = Math.log(2) / HALF_LIFE_PERIOD;
   	return Math.ceil(Math.log(act) / k);
   }
-  return false;	
-  
+  return false;
+
 };
