f4da3cf7733bdda3eb276a53a02232bee265dae2
roshik14 <fraust1337@gmail.com>
2021-03-02 10:23:47 +0700

    fix: change solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,44 +2,43 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw new Error('argument is not an array');
-  const transformedArr = [];
+  let transformedArr = [];
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
 
   for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === discardNext || arr[i] === discardPrev
+        || arr[i] === doubleNext || arr[i] === doublePrev ) continue;
 
-    if (arr[i] === '--discard-next' || arr[i - 1] === '--discard-next') {
+    if (arr[i - 1] === discardNext || arr[ + 1] === discardPrev) {
       continue;
-    }
+    } else if (arr[i - 1] === doubleNext) {
+      if (arr[i + 1] === doublePrev) {
+        transformedArr.push(arr[i], arr[i], arr[i]);
+        continue;
+      } else if (arr[i + 1] === discardPrev) {
+        transformedArr.push(arr[i]);
+        continue;
+      } else {
+        transformedArr.push(arr[i], arr[i]);
+        continue;
+      }
 
-    if (arr[i] === '--double-next') {
-      if (arr[i + 1] === undefined || arr[i + 2] === undefined) {
-        continue;
-      }
-      if (arr[i + 2] == '--discard-prev') {
-        continue;
-      }
-      transformedArr.push(arr[i + 1]);
+    } else if (arr[i + 1] === discardPrev || arr[i - 1] === discardNext) {
       continue;
-    }
 
-    if (arr[i] === '--discard-prev') {
+    } else if (arr[i + 1] === doublePrev) {
+      if (arr[i - 1] === discardNext) {
       continue;
-    }
-
-    if (arr[i] === '--double-prev') {
-      if (arr[i - 2] === '--double-next') {
-        transformedArr.push(arr[i - 1]);
-        continue;
       }
-      if( arr[i - 1] === undefined) {
-        continue;
-      }
-      if( arr[i - 2] === '--discard-next') {
-        continue;
-      }
-      transformedArr.push(arr[i -1], arr[i - 1]);
+      transformedArr.push(arr[i], arr[i]);
       continue;
-    }
+    } else {
     transformedArr.push(arr[i]);
+    }
   }
+  console.log(arr);
   return transformedArr;
 };
